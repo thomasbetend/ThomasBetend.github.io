e577963d851fc7b7a06fecd8852eb9e81087dd95
thomasbetend <thomas.betend@yahoo.fr>
2022-09-27 13:51:20 +0200

    total refresh

--- index.js
+++ index.js
@@ -1,30 +1,8 @@
+var logoPrincipal = document.getElementById("logo-principal");
+var btnPrincipal = document.getElementById("btn-principal");
 
+window.onload= ()=>{
+    logoPrincipal.classList.add("logo-appear");
+    btnPrincipal.classList.add("btn-appear");
+}
 
-var openBtn = document.getElementById('icone-menu-b');
-var closeBtn = document.getElementById('icone-menu-h');
-var sideMenu = document.getElementById('menu-hide');
-
-openBtn.addEventListener("click", ()=>{
-    sideMenu.classList.add("active");
-        })
-
-closeBtn.addEventListener("click", ()=>{
-            sideMenu.classList.remove("active");
-                })
-
-/* Curseur spécial. 
-
-const cursor = document.querySelector('.cursor');
-
-document.addEventListener("mousemove", (e)=>{
-    cursor.setAttribute('style', 'top:'+(e.pageY - 15)+'px; left:'+(e.pageX - 15)+'px;')
-})
-
-document.addEventListener("click", ()=>{
-    cursor.classList.add('expand');
-
-    setTimeout(()=>{
-        cursor.classList.remove('expand');
-    }, 500);
-})
-*/
