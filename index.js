a05057be2ba5a7c5b0fd2fdfbdc4f180145fac51
thomasbetend <thomas.betend@yahoo.fr>
2022-09-28 11:28:47 +0200

    add new home page

---
+++ index.js
@@ -0,0 +1,8 @@
+var logoPrincipal = document.getElementById("logo-principal");
+var btnPrincipal = document.getElementById("btn-principal");
+
+window.onload= ()=>{
+    logoPrincipal.classList.add("logo-appear");
+    btnPrincipal.classList.add("btn-appear");
+}
+
